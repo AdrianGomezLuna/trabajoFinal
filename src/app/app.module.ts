728b51cbccdfd818414bd9fa884f39d57f2a7bbd
AdrianGomezLuna <adrigoluna@gmail.com>
2021-11-26 09:26:06 +0100

    Estructura

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NuevoComponent } from './components/nuevo/nuevo.component';
+import { EmpleadosComponent } from './components/empleados/empleados.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NuevoComponent,
+    EmpleadosComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
